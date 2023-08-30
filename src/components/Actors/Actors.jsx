9a48638481f2fbac5816f5138c710bf1c35f3a57
eyosolutions <emmanuel.y.odoom@gmail.com>
2023-08-30 13:02:53 +0000

    file and folder structure

---
+++ src/components/Actors/Actors.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Actors = () => {
+  console.log('info');
+  return (
+    <div>
+      Actors
+    </div>
+  );
+};
+
+export default Actors;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,36 @@
 // rafce
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+import { Actors, Movies, MovieInformation, NavBar, Profile } from '.';
+
+import useStyles from './styles';
+
 const App = () => {
-  <div>
-    App
-  </div>
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      {/* <NavBar /> */}
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/movies/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 };
 export default App;
